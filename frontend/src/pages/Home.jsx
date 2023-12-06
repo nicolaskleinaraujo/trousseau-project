04e287221b5f5dd277d41576694221f6a7c58b48
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2023-12-05 22:21:50 -0300

    finished for today

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,23 @@
 import styles from "./Home.module.css"
 
+import { useEffect, useState } from "react"
+import dbFetch from "../axios/config"
+
 function Home() {
+    const [items, setItems] = useState([])
+
+    const getItems = async() => {
+        try {
+            // const response = await dbFetch.get()
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        getItems()
+    }, [])
+
     return (
         <div className={styles.home}>
             <h1>Projeto de Enxoval</h1>
@@ -8,10 +25,6 @@ function Home() {
                 <div>
                     <h2>Cozinha</h2>
                     <ul>
-                        <li>Opção 1</li>
-                        <li>Opção 2</li>
-                        <li>Opção 3</li>
-                        <li>Opção 4</li>
                     </ul>
                 </div>
                 <div>
