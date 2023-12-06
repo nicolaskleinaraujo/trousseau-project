8e6e858cdeb9831717ad8970b421f288166fe88e
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2023-12-05 22:15:01 -0300

    made the css for some files

--- frontend/src/pages/Add.jsx
+++ frontend/src/pages/Add.jsx
@@ -1,50 +1,55 @@
+import styles from "./Add.module.css"
+
 import dbFetch from "../axios/config"
-
 import { useState } from "react"
-
 import { useNavigate } from "react-router-dom"
 
 function Add() {
     const navigate = useNavigate()
+
     const [name, setName] = useState()
     const [place, setPlace] = useState()
 
     const createItem = async (e) => {
         e.preventDefault()
+
         await dbFetch.post("/items", {
             name,
             place
         })
+
         navigate("/")
     }
 
     return (
-        <div>
-            <h1>Add</h1>
+        <div className={styles.add}>
+            <h1>Adicionar Item</h1>
             <form onSubmit={(e) => createItem(e)}>
                 <div>
-                    <label htmlFor="name">Nome do Item: </label>
+                    <label htmlFor="name">Nome: </label>
                     <input 
                     type="text" 
                     name="name" 
                     id="name" 
                     required 
+                    placeholder="Digite o nome"
                     onChange={(e) => setName(e.target.value)} 
                     />
                 </div>
                 <div>
-                    <label htmlFor="place">Local do Item: </label>
+                    <label htmlFor="place">Local: </label>
                     <input 
                     type="text" 
                     name="place" 
                     id="place" 
                     required
+                    placeholder="Digite o local"
                     onChange={(e) => setPlace(e.target.value)} 
                     />
                 </div>
-                <input type="submit" value="Adicionar" />
+                <input type="submit" value="Adicionar" className={styles.submit_button} />
             </form>
-        </div>  
+        </div>
     )
 }
 
