966abf4717db292caade067c56d7932fc2a2b06a
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2023-12-07 16:41:28 -0300

    added some text to the footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,7 +3,10 @@ import styles from "./Footer.module.css"
 function Footer() {
     return (
         <footer className={styles.footer}>
-            <p>&copy; 2023 - Site desenvolvido por <a href="https://github.com/nicolaskleinaraujo/" target="blank">Nicolas Klein</a></p>
+            <p>&copy; 2023 - Site desenvolvido por <a href="https://github.com/nicolaskleinaraujo/" target="blank">Nicolas Klein</a>.</p>
+            <p>Este site foi feito para estudo, por favor utilize com moderação!</p>
+            <p>Site desenvolvido utilizando React. A API utilizada para a comunicação com o banco de dados foi feito utilizando Node.js.</p>
+            <p>Você pode conferir o <a href="#" target="blank">repositorio</a> deste site para ver o codigo fonte!</p>
         </footer>
     )
 }
