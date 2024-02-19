531b250e7a2b0047cf6ab4e7f7c3f9b4a8c46ca8
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-02-19 15:45:07 -0300

    not finished - check item for home2

--- frontend/src/pages/Home2/Home2.jsx
+++ frontend/src/pages/Home2/Home2.jsx
@@ -1,9 +1,84 @@
+// CSS
+import styles from "./Home2.module.css"
+
+// Icons
+import { FaTrash, FaCheck } from "react-icons/fa"
+
+// DB Fetch
+import dbFetch from "../../axios/config"
+
+// Modules
+import { useEffect, useState } from "react"
+
 const Home2 = () => {
-  return (
-    <div>
-        <h2>HOME2</h2>
-    </div>
-  )
+    const [items, setItems] = useState([])
+    const getItems = async() => {
+        try {
+            const response = await dbFetch.get("/items")
+            setItems(response.data)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    const checkItem = async(id) => {
+        var itemCheck = items.filter((item) => item._id === id)
+        if (itemCheck.check == "true") {
+            console.log("true")
+            /*itemCheck.check = "false"
+
+            await dbFetch.put(`/items/${id}`, {
+                check: "false"
+            }) */
+        } else if (itemCheck.check == "false") {
+            console.log("false")
+            /*itemCheck.check = "true"
+
+            await dbFetch.put(`/items/${id}`, {
+                check: "true"
+            })*/
+        }
+    }
+
+    useEffect(() => {
+        getItems()
+    }, [])
+
+    console.log(items)
+
+    return (
+        <div>
+            <h1>Enxoval</h1>
+            <div className={styles.grid_areas}>
+                <div>
+                    {items &&
+                        items.map((item) => (
+                            item.check === "true" ? (
+                                <div id={item._id} >
+                                    <li className={styles.checked}>{item.name} --- {item.place}</li>
+                                    <button onClick={() => checkItem(item._id)}>teste</button>
+                                </div>
+                            ) : (
+                                <div id={item._id} >
+                                    <li>{item.name} --- {item.place}</li>
+                                    <button onClick={() => checkItem(item._id)}>teste</button>
+                                </div>
+                            )
+                        ))
+                    }
+                </div>
+                <div>
+
+                </div>
+                <div>
+
+                </div>
+                <div>
+
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Home2
