a1a7588b26dff580a1ee1f133c3a5cc28bff32a1
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-04-02 16:49:22 -0300

    added getItem function on the updateItem page

--- frontend/src/pages/UpdateItem/UpdateItem.jsx
+++ frontend/src/pages/UpdateItem/UpdateItem.jsx
@@ -1,11 +1,31 @@
-import styles from "./UpdateItem.css"
+// CSS
+import styles from "./UpdateItem.module.css"
+
+// DB Fetch
+import dbFetch from "../../axios/config"
+
+// Modules
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
 
 const UpdateItem = () => {
-  return (
-    <div>
-        <h1>Update Item</h1>
-    </div>
-  )
+    const { id } = useParams()
+
+    const [item, setItem] = useState()
+    const getItem = async() => {
+        const res = await dbFetch.get(`/items/${id}`)
+        setItem(res.data)
+    }
+
+    useEffect(() => {
+        getItem()
+    }, [])
+
+    return (
+        <div>
+            <h1>Atualizar Item</h1>
+        </div>
+    )
 }
 
 export default UpdateItem
